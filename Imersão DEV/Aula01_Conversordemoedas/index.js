265937ce250c2c3296609e926f3514e463796599
Jose <bakuja_dev@outlook.com>
2024-02-03 00:11:09 -0300

    Arquivos de estudo

---
+++ Imersão DEV/Aula01_Conversordemoedas/index.js
@@ -0,0 +1,9 @@
+let valorEmDolar = 30;
+let cotacaoDodolar = 5.32;
+let valorEmReal = valorEmDolar * cotacaoDodolar;
+console.log (valorEmDolar);
+
+valorEmReal = valorEmReal.toFixed(2);
+alert ("R$" + valorEmReal)
+
+/*matchAround ou .toFixed*/
