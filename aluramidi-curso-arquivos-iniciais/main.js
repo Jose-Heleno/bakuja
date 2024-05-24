4d08bd779eff7bf7e2f575471d984ab18f2e40e6
Jose <bakuja_dev@outlook.com>
2024-05-24 12:27:47 -0300

    alteração de repositorio

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,18 @@
-alert ('Olá  Mundo');
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listKeys = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < listKeys.length; contador++) {
+
+    const tecla = listKeys[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; // template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+    
+}
