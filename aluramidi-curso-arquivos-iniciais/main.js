c004f40a8a91a1bbd8f3c8938cadfe4138907401
Jose <bakuja_dev@outlook.com>
2024-06-27 13:22:11 -0300

    Alterações de arquivos

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,14 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-}
+function tocaSom (selectorAudio) {
+    const elemento = document.querySelector(selectorAudio);
+
+        if (elemento && elemento.localName === 'audio' ) {
+                elemento.play();
+            }
+            else {
+                console.log ('Elemento não encontrado ou seletor inválido.');
+            }
+        }
+    
 
 const listKeys = document.querySelectorAll('.tecla');
 
@@ -12,7 +20,20 @@ for (let contador = 0; contador < listKeys.length; contador++) {
     const idAudio = `#som_${instrumento}`; // template string
 
     tecla.onclick = function () {
-        tocaSom(idAudio)
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+    } 
+
+    tecla.onkeyup = function () {
+
+        tecla.classList.remove('ativa'); 
     }
     
+}
+
 }
